de77d626125557da03ec831499187e5248905ba4
SanishPoudel <mr.sanish.poudel@gmail.com>
2023-12-29 12:56:45 +1100

    Added get_valid_words function

--- hangman.py
+++ hangman.py
@@ -3,3 +3,9 @@ from hangman_visual import lives_visual_dict
 import string
 import random
 
+def get_valid_words(words):
+    """Function to get valid words without spaces or hyphes from the list of random words."""
+    valid_words = random.choice(words)
+    while " " in valid_words or "-" in valid_words:
+        valid_words = random.choice(words)
+    return valid_words.upper()
